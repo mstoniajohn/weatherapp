8811d68f7805d305aed18085a04996dfe4cbfee9
github username <github email address>
2021-03-23 13:23:15 -0400

    fix useEffect dependables

--- src/App.js
+++ src/App.js
@@ -26,73 +26,79 @@ function App() {
 		setCity(search);
 		setSearch('');
 	};
-	const getWeather = async () => {
-		fetch(
-			`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-		)
-			.then((response) => response.json())
-			.then((data) => {
-				// return data;
-				getTime(data.coord.lat, data.coord.lon);
-				setWeather(data);
-				// getDregees(data.wind.deg);
-				let deg = Math.floor(data.wind.deg);
-				switch (true) {
-					case deg >= 360 && deg <= 21:
-						deg = 'N';
-						break;
-					case deg >= 22 && deg <= 44:
-						deg = 'NNE';
-						break;
-					case deg >= 45 && deg <= 66:
-						deg = 'NE';
-						break;
-					case deg >= 67 && deg <= 89:
-						deg = 'ENE';
-						break;
-					case deg >= 90 && deg <= 111:
-						deg = 'E';
-						break;
-					case deg >= 112 && deg <= 134:
-						deg = 'ESE';
-						break;
-					case deg >= 135 && deg <= 156:
-						deg = 'SE';
-						break;
-					case deg >= 157 && deg <= 179:
-						deg = 'SSE';
-						break;
-					case deg >= 180 && deg <= 201:
-						deg = 'S';
-						break;
-					case deg >= 202 && deg <= 224:
-						deg = 'SSW';
-						break;
-					case deg >= 225 && deg <= 246:
-						deg = 'SW';
-						break;
-					case deg >= 247 && deg <= 269:
-						deg = 'WSW';
-						break;
-					case deg >= 270 && deg <= 291:
-						deg = 'W';
-						break;
-					case deg >= 292 && deg <= 314:
-						deg = 'WNW';
-						break;
-					case deg >= 315 && deg <= 336:
-						deg = 'NW';
-						break;
-					case deg >= 337 && deg <= 359:
-						deg = 'NNW';
-						break;
-					default:
-						deg = 'no data';
-				}
-				setDegree(deg);
-			})
-			.catch((err) => console.log(err));
-	};
+	useEffect(() => {
+		document.getElementById('input').focus();
+
+		const getWeather = async () => {
+			fetch(
+				`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+			)
+				.then((response) => response.json())
+				.then((data) => {
+					// return data;
+					getTime(data.coord.lat, data.coord.lon);
+					setWeather(data);
+					// getDregees(data.wind.deg);
+					let deg = Math.floor(data.wind.deg);
+					switch (true) {
+						case deg >= 360 && deg <= 21:
+							deg = 'N';
+							break;
+						case deg >= 22 && deg <= 44:
+							deg = 'NNE';
+							break;
+						case deg >= 45 && deg <= 66:
+							deg = 'NE';
+							break;
+						case deg >= 67 && deg <= 89:
+							deg = 'ENE';
+							break;
+						case deg >= 90 && deg <= 111:
+							deg = 'E';
+							break;
+						case deg >= 112 && deg <= 134:
+							deg = 'ESE';
+							break;
+						case deg >= 135 && deg <= 156:
+							deg = 'SE';
+							break;
+						case deg >= 157 && deg <= 179:
+							deg = 'SSE';
+							break;
+						case deg >= 180 && deg <= 201:
+							deg = 'S';
+							break;
+						case deg >= 202 && deg <= 224:
+							deg = 'SSW';
+							break;
+						case deg >= 225 && deg <= 246:
+							deg = 'SW';
+							break;
+						case deg >= 247 && deg <= 269:
+							deg = 'WSW';
+							break;
+						case deg >= 270 && deg <= 291:
+							deg = 'W';
+							break;
+						case deg >= 292 && deg <= 314:
+							deg = 'WNW';
+							break;
+						case deg >= 315 && deg <= 336:
+							deg = 'NW';
+							break;
+						case deg >= 337 && deg <= 359:
+							deg = 'NNW';
+							break;
+						default:
+							deg = 'no data';
+					}
+					setDegree(deg);
+				})
+				.catch((err) => console.log(err));
+		};
+		getWeather();
+	}, [city]);
+
 	// const speed = (data) => {
 	// 	return Number(data);
 	// };
@@ -142,11 +148,7 @@ function App() {
 				// return data.formatted || 'hi';
 			});
 	};
-	useEffect(() => {
-		document.getElementById('input').focus();
 
-		getWeather();
-	}, [city]);
 	return (
 		<>
 			<main>
